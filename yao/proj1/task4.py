3ffc87094c77a5241cd643dceca7ce38f60a78fb
yao-lin-902 <uygwfd254@gmail.com>
2023-03-23 13:35:02 -0700

    cleaner turning machine implementation

--- yao/proj1/task4.py
+++ yao/proj1/task4.py
@@ -1,54 +1,65 @@
+tm = {
+    "START": {">": ["GORIGHT", "R", ""]},
+    "GORIGHT": {
+        ".": ["OUTPUT0", "L", ""],
+        "0": ["FOUND0_1", "R", "."],
+        "1": ["FOUND1_1", "R", "."],
+    },
+    "FOUND0_1": {
+        ".": ["OUTPUT0", "L", ""],
+        "0": ["FOUND0_1", "R", "."],
+        "1": ["FOUND1_1", "R", "."],
+    },
+    "FOUND1_1": {
+        ".": ["OUTPUT0", "L", ""],
+        "0": ["FOUND1_1", "R", "."],
+        "1": ["FOUND2_1", "R", "."],
+    },
+    "FOUND2_1": {
+        ".": ["OUTPUT0", "L", ""],
+        "0": ["FOUND2_1", "R", "."],
+        "1": ["FOUND3_1", "R", "."],
+    },
+    "FOUND3_1": {
+        ".": ["OUTPUT1", "L", ""],
+        "0": ["FOUND3_1", "R", "."],
+        "1": ["FOUND3_1", "R", "."],
+    },
+    "OUTPUT0": {".": ["OUTPUT0", "L", ""], ">": ["WRITE0", "R", ""]},
+    "OUTPUT1": {".": ["OUTPUT1", "L", ""], ">": ["WRITE1", "R", ""]},
+    "WRITE0": {".": ["WRITE0", "H", "0"]},
+    "WRITE1": {".": ["WRITE1", "H", "1"]},
+}
+
 starting_state = "START"
 
 
-def at_least_3_ones(state, sym, move=""):
-    if sym == ">" and state == "START":
-        state, move = "LEFTMOST", "R"
-    elif state == "LEFTMOST":
-        if sym == ".":
-            state, move = "OUTPUT0", "H"
-        else:
-            state = "FOUND0" if sym == "0" else "FOUND1"
-            move = "R"
-    elif sym == "1" and state == "FOUND0":
-        state, move = "FOUND1", "R"
-    elif sym == "1" and state == "FOUND1":
-        state, move = "FOUND2", "R"
-    elif sym == "1" and state == "FOUND2":
-        state, move = "FOUND3", "R"
-    elif state == "FOUND3":
-        if sym == ".":
-            state, move = "OUTPUT1", "H"
-    elif sym == "." and state in ["FOUND0", "FOUND1", "FOUND2"]:
-        state, move = "OUTPUT0", "H"
-    else:
-        state, move = state, "R"
-
-    return state, move
-
-
-def TM(x):
+def M(x):
     tape = ">" + x + "."
     i = 0
     state = starting_state
 
     while True:
-        state, move = at_least_3_ones(state, tape[i])
+        # print(tape)
+        state, move, sym = tm[state][tape[i]]
 
-        if move == "R":
-            i += 1
-        elif move == "L":
-            i -= 1
-        else:
-            break
+        if sym:
+            tape = tape[:i] + sym + tape[i + 1 :]
 
-    if state == "OUTPUT0":
-        return "0"
-    elif state == "OUTPUT1":
-        return "1"
+        match move:
+            case "L":
+                i -= 1
+            case "R":
+                i += 1
+            case "S":
+                i += 0
+            case "H":
+                break
+
+    return tape[1 : tape.index(".")]
 
 
-result = TM("10001")
+result = M("1000101")
 
 if result == "1":
     print("Accepted")
